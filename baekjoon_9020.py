df9e50a88499138b32d0f204b7f5aef3183ba229
Sungjune Lee <ljhkdoryb9@gmail.com>
2021-11-15 16:12:49 +0900

    9020

---
+++ baekjoon_9020.py
@@ -0,0 +1,48 @@
+# 1보다 큰 자연수 중에서  1과 자기 자신을 제외한 약수가 없는 자연수를 소수라고 한다. 
+# 예를 들어, 5는 1과 5를 제외한 약수가 없기 때문에 소수이다. 
+# 하지만, 6은 6 = 2 × 3 이기 때문에 소수가 아니다.
+# 골드바흐의 추측은 유명한 정수론의 미해결 문제로, 
+# 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다는 것이다. 
+# 이러한 수를 골드바흐 수라고 한다. 
+# 또, 짝수를 두 소수의 합으로 나타내는 표현을 그 수의 골드바흐 파티션이라고 한다. 
+# 예를 들면, 
+# 4 = 2 + 2, 
+# 6 = 3 + 3, 
+# 8 = 3 + 5, 
+# 10 = 5 + 5, 
+# 12 = 5 + 7, 
+# 14 = 3 + 11, 
+# 14 = 7 + 7이다. 
+# 10000보다 작거나 같은 모든 짝수 n에 대한 골드바흐 파티션은 존재한다.
+# 2보다 큰 짝수 n이 주어졌을 때, n의 골드바흐 파티션을 출력하는 프로그램을 작성하시오. 
+# 만약 가능한 n의 골드바흐 파티션이 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
+# 각 테스트 케이스에 대해서 주어진 n의 골드바흐 파티션을 출력한다.
+# 출력하는 소수는 작은 것부터 먼저 출력하며, 공백으로 구분한다.
+
+def find_prime_list(max_n):
+    stop_n = int(max_n ** 0.5)
+    tf_list = [True] * (max_n - 1)
+    for i in range(2, stop_n + 1):
+        if tf_list[i - 2] == True:
+            for j in range(i * 2, max_n + 1, i):
+                tf_list[j - 2] = False
+    prime_num = [
+        i for i in range(2, max_n + 1)
+            if (tf_list[i - 2] == True)
+    ]
+    return prime_num
+
+def find_min_goldbach_num(num, prime_10000_list):
+    prime_list = [i for i in prime_10000_list if i <= num]
+    half_over_p_list = [i for i in prime_list if i >= (num // 2)]
+    for n in half_over_p_list:
+        under_p_list = [i for i in prime_list if i <= num - n][-1::-1]
+        for m in under_p_list:
+            if n + m == num:
+                return print(m, n)
+            elif n + m < num:
+                break
+            
+prime_10000_list = find_prime_list(10000)
+for _ in range(int(input())):
+    find_min_goldbach_num(int(input()), prime_10000_list)
